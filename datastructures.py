8d7fe87f11ff9561f5e6d7e461386094b904a8b5
Raman Z <rzv090701@gmail.com>
2023-05-01 22:44:28 -0400

    lists

--- datastructures.py
+++ datastructures.py
@@ -1,1 +1,26 @@
 # data structures go here
+
+def list_methods():
+    fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+    print("There are " + str(fruits.count('apple')) + " apples")
+
+    print("Index of banana:", fruits.index('banana'))
+
+    print("Next banana starting at position 4:", fruits.index('banana', 4))
+
+    fruits.reverse()
+    fruits.append('grape')
+    fruits.sort()
+    fruits.pop()
+    print(fruits)
+
+list_methods()
+
+def list_comprehension():
+    squares = [x**2 for x in range(10)]
+    print(squares)
+
+    combined_elements = [(x,y) for x in [1,2,3] for y in [3,1,4] if x != y]
+    print(combined_elements)
+
+list_comprehension()
